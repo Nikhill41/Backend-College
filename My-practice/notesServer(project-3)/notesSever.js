ea62d14a8f647739179fe3d40294d2e17e79877d
Nikhil Pal <nikhilpal630622@gmail.com>
2026-02-18 13:45:08 +0530

    delted md

--- My-practice/notesServer(project-3)/notesSever.js
+++ My-practice/notesServer(project-3)/notesSever.js
@@ -1,1 +1,26 @@
-const  httpp=require('fs')
+const  http=require('http');
+const  fs=require('fs');
+
+const server=http.createServer((req,res)=>{
+    const method=req.method;
+    const  url=req.url;
+    fs.readFile("./notes.json", 'utf-8', (err, data) => {
+        if(err) data = '[]';
+
+    if(method=="POST" && url=="/data"){
+        req.accepts();
+
+    }else if(method=="GET" && url=="/data"){
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.end(JSON.stringify(data));
+    }else{
+        res.writeHead(404,{"Content-Type":"text/plain"});
+        res.end("route not fond");
+    }
+    });
+});
+
+
+server.listen(3400, () => {
+    console.log('server listening on port 3000!');
+});
